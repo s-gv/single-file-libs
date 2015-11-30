494e6440fd4a0c355bb87ebb4d3f15644c606a3c
Sagar G V <sagar.writeme@gmail.com>
2015-11-30 13:02:43 +0530

    New API for reading JSON

--- sgv_json.h
+++ sgv_json.h
@@ -13,32 +13,10 @@ NOTES
 - This library performs no memory allocation.
 - See http://www.json.org/ for the JSON spec.
 - Notation of pair, value, array, object, etc. are followed from the spec.
+- Tokens contain pointers to the parsed JSON string (so don't de-allocate it)
 
 EXAMPLE
 -------
-
-char* json_str = "{a: 2, b: [3, 6, 9], c: true}";
-int json_str_len = strlen(json_str);
-
-sgv_json_object obj;
-unsigned char scratch_pad[100];
-sgv_json_parse_object(json_str, json_str_len, scratch_pad, 100, &obj);
-
-sgv_json_value val;
-sgv_json_get_value(&obj, "a", &val);
-
-int a;
-sgv_json_get_int(&val, &a); // a will be 2
-
-sgv_json_get_value(&obj, "b", &val);
-sgv_json_array arr;
-sgv_json_get_array(&val, &arr);
-
-sgv_json_element ele1, ele2;
-sgv_json_get_first_element(&arr, &ele1);
-sgv_json_get_next_element(&ele1, &ele2);
-sgv_json_get_element_value(&ele2, &val);
-sgv_json_get_int(&val, &a); // a will be 6
 
 OPTIONS
 -------
@@ -57,8 +35,6 @@ is expressed or implied.
 
 */
 
-/*****************************************************************************
-****************************** Public API ***********************************/
 #ifndef SGV_JSON_H
 #define SGV_JSON_H
 
@@ -72,90 +48,91 @@ extern "C" {
 #define SGVJSON_DEF extern
 #endif
 
-typedef struct sgv_json_object;
-typedef struct sgv_json_array;
-typedef struct sgv_json_pair;
-typedef struct sgv_json_value;
-typedef struct sgv_json_string {
-    char* str; // May not be null terminated!
-    int n_chars;
-} sgv_json_string;
+/*****************************************************************************
+****************************** Public API ***********************************/
 
-typedef enum {
-    SGV_JSON_VALUE_STRING,
-    SGV_JSON_VALUE_NUMBER,
-    SGV_JSON_VALUE_OBJECT,
-    SGV_JSON_VALUE_ARRAY,
-    SGV_JSON_VALUE_TRUE,
-    SGV_JSON_VALUE_FALSE,
-    SGV_JSON_VALUE_NULL
-} sgv_json_value_type;
+typedef struct sgv_json_token;
 
-typedef enum {
-    SGV_JSON_ERROR_PARSE_FAIL = -1,
-    SGV_JSON_ERROR_INCORRECT_VALUE_TYPE = -2,
-} sgv_json_error_type;
-
-/* returns 0 on success, negative if error, and number of bytes (positive)
-   if the size of scratch_pad is in-sufficient */
+/* returns 0 on success, negative if error, and number of tokens (positive)
+   if the size of scratch_pad is in-sufficient. The root JSON obj will be in
+   tokens[0]. */
 SGVJSON_DEF int sgv_json_parse_object(char* json_str,
                                       int json_str_len,
-                                      unsigned char* scratch_pad,
-                                      int scratch_pad_len,
-                                      sgv_json_object* obj);
+                                      sgv_json_token* tokens,
+                                      int max_tokens);
 
-/* looks for pair by key.
-   Returns NULL if there is no pair with matching key */
-SGVJSON_DEF void sgv_json_get_value(sgv_json_object* obj,
-                                    char* key,
-                                    sgv_json_value* value);
-
-/* returns the first pair in the JSON object, NULL if the object is empty */
-SGVJSON_DEF void sgv_json_get_first_pair(sgv_json_object* obj,
-                                         sgv_json_pair* pair);
-
-/* returns the next pair in the JSON object, NULL if no more pairs are left */
-SGVJSON_DEF void sgv_json_get_next_pair(sgv_json_pair* current_pair,
-                                        sgv_json_pair* next_pair);
-
-/* returns the string corresponding to the key of this pair */
-SGVJSON_DEF sgv_json_string sgv_json_get_pair_key(sgv_json_pair* pair);
-
-/* returns the value of this pair */
-SGVJSON_DEF sgv_json_value sgv_json_get_pair_value(sgv_json_pair* pair);
-
-/* returns the type of the value */
-SGVJSON_DEF int sgv_json_get_value_type(sgv_json_value* value);
-
-/* returns 0 on successfully reading value,
-   SGV_JSON_ERROR_INCORRECT_VALUE_TYPE if that value type cant be read.
-   Boolean value: 0-false, 1-true */
-SGVJSON_DEF int sgv_json_get_int(sgv_json_value* val, int* number);
-SGVJSON_DEF int sgv_json_get_float(sgv_json_value* val, float* number);
-SGVJSON_DEF int sgv_json_get_double(sgv_json_value* val, double* number);
-SGVJSON_DEF int sgv_json_get_bool(sgv_json_value* val, int* bool_val);
-SGVJSON_DEF int sgv_json_get_string(sgv_json_value* v, sgv_json_string* str);
-SGVJSON_DEF int sgv_json_get_object(sgv_json_value* v, sgv_json_object* obj);
-SGVJSON_DEF int sgv_json_get_array(sgv_json_value* val, sgv_json_array* arr);
-
-/* returns 0 on success, negative if error, and number of bytes (positive)
-   if the size of scratch_pad is in-sufficient */
+/* returns 0 on success, negative if error, and number of tokens (positive)
+   if the size of scratch_pad is in-sufficient. The root JSON array will be in
+   tokens[0]. */
 SGVJSON_DEF int sgv_json_parse_array(char* json_str,
                                      int json_str_len,
-                                     unsigned char* scratch_pad,
-                                     int scratch_pad_len,
-                                     sgv_json_array* arr);
+                                     sgv_json_token* tokens,
+                                     int max_tokens);
 
-/* returns the first element in the JSON array, NULL if the array is empty */
-SGVJSON_DEF void sgv_json_get_first_element(sgv_json_array* arr,
-                                            sgv_json_element* element);
+/* Returns first pair in the obj, NULL if the JSON object is empty/on error */
+SGVJSON_DEF sgv_json_token* sgv_json_first_pair(sgv_json_token* obj);
 
-/* returns the next element in the JSON object, NULL if no more are left */
-SGVJSON_DEF void sgv_json_get_next_element(sgv_json_element* current_element,
-                                           sgv_json_element* next_element);
+/* Returns the next pair in the JSON object, NULL if there are none left */
+SGVJSON_DEF sgv_json_token* sgv_json_next_pair(sgv_json_token* current_pair);
 
-/* returns the value of this pair */
-SGVJSON_DEF sgv_json_value sgv_json_get_element_value(sgv_json_element* ele);
+/* Returns first element in array, NULL if the JSON array is empty/on error */
+SGVJSON_DEF sgv_json_token* sgv_json_first_element(sgv_json_token* arr);
+
+/* Returns next element in array, NULL if there are none left/on error */
+SGVJSON_DEF sgv_json_token* sgv_json_next_element(sgv_json_token* arr);
+
+/* Returns the key corresponding to 'pair'. NULL if error */
+SGVJSON_DEF sgv_json_token* sgv_json_pair_key(sgv_json_token* pair);
+
+/* Returns the value corresponding to 'pair'. NULL if error */
+SGVJSON_DEF sgv_json_token* sgv_json_pair_value(sgv_json_token* pair);
+
+/* Returns the value corresponding to 'element'. NULL if error */
+SGVJSON_DEF sgv_json_token* sgv_json_element_value(sgv_json_token* element);
+
+/* Scans the JSON object for a pair with matching key and retrieves
+   the *value* of the pair. Returns NULL if no such pair exists / on error */
+SGVJSON_DEF sgv_json_token* sgv_json_obj_value(sgv_json_token* obj, char* key);
+
+/* Scans the JSON array and retrieves the *value* at index 'idx'.
+   Returns NULL if no such element exists / on error */
+SGVJSON_DEF sgv_json_token* sgv_json_arr_value(sgv_json_token* arr, int idx);
+
+/* returns 0 on success. negative on error */
+SGVJSON_DEF int sgv_json_key_string(sgv_json_token* pair_key,
+                                    char **str, int *str_len);
+
+/* returns JSON obj on success, NULL if error */
+SGVJSON_DEF sgv_json_token* sgv_json_value_obj(sgv_json_token* value);
+
+/* returns JSON array on success, NULL if error */
+SGVJSON_DEF sgv_json_token* sgv_json_value_array(sgv_json_token* value);
+
+/* returns 0 on success. negative on error */
+SGVJSON_DEF int sgv_json_value_string(sgv_json_token* value,
+                                      char **str, int *str_len);
+
+/* returns 0 on success. negative on error */
+SGVJSON_DEF int sgv_json_value_int(sgv_json_token* value, int* number);
+
+/* returns 0 on success. negative on error */
+SGVJSON_DEF int sgv_json_value_double(sgv_json_token* value, double* number);
+
+/* returns 0 on success. negative on error. bool_val = 1 if True, else 0 */
+SGVJSON_DEF int sgv_json_value_bool(sgv_json_token* value, int* bool_val);
+
+/* returns 0 on success. negative on error. is_null = 1 if Null, else 0 */
+SGVJSON_DEF int sgv_json_value_null(sgv_json_token* value, int* is_null);
+
+/*****************************************************************************
+*****************************************************************************/
+
+/* WARNING: Do *not* access members of this struct directly.
+   This is a private struct whose definition has been exposed to facilitate
+   allocation of tokens on the stack (or statically) */
+typedef struct sgv_json_token {
+    int type;
+} sgv_json_token;
 
 #ifdef __cplusplus
 }
