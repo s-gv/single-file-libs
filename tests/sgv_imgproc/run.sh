cc06ce9802c67195c70ac3d9e62a99442945f444
Sagar G V <sagar.writeme@gmail.com>
2016-06-06 11:04:38 +0530

    C flag to check for leaks

--- tests/sgv_imgproc/run.sh
+++ tests/sgv_imgproc/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -std=c89 -pedantic -Wall -O2 -I../../ test.c -o out -lm
+gcc -std=c89 -pedantic -Wall -O2 -fsanitize=address -fno-omit-frame-pointer -I../../ test.c -o out -lm
 ./out
 rm -f out
